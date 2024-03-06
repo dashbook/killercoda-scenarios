8ae300947c92ef9b65e0c364910d793c9f90f80d
Jan Kaul <jankaul@mailbox.org>
2024-03-06 17:15:30 +0100

    extract and load

--- dashtool-postgres/background.sh
+++ dashtool-postgres/background.sh
@@ -1,3 +1,4 @@
 echo "Installing dashtool..."
 wget -qO- https://github.com/dashbook/dashtool/releases/download/0.1.0/dashtool_0.1.0_x86_64-unknown-linux-musl.tar.gz | tar xvz
 echo DONE
+mv gitignore .gitignore
